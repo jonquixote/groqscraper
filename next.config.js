b6ec62c0c8023240f4ebd2fc000b14df53e20ff5
John Hawley <johnny@Johns-MBP.lan>
2025-03-19 02:04:44 -0700

    7

--- next.config.js
+++ next.config.js
@@ -7,6 +7,16 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, { isServer }) => {
+    // Fix for undici package
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        "undici": false
+      };
+    }
+    return config;
+  },
 }
 
 module.exports = nextConfig
